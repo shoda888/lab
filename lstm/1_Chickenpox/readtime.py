ef45013e0706b8cf925466830b58ad6d3cff8ad6
shoda888 <gekka888@gmail.com>
2020-05-21 00:36:20 +0900

    fix header bug

--- lstm/1_Chickenpox/readtime.py
+++ lstm/1_Chickenpox/readtime.py
@@ -3,7 +3,8 @@ import pandas as pd
 def showtime(x):
     return str(x)[8:10] +':'+ str(x)[10:12] +':'+ str(x)[12:14]
 
-df = pd.read_csv('0520exData/training/0/pos.csv')
+df = pd.read_csv('0520exData/training/0/pos.csv', header=None)
+print(len(df))
 time_df = df.iloc[:, 0].map(showtime)
-
+print(len(time_df))
 time_df.to_csv('timeindex.csv', header=False, index=False)
