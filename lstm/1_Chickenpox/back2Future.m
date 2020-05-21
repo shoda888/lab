29e6e36b18bdc66dc0bd6a258f2b6d1d2ffa9d45
shoda888 <gekka888@gmail.com>
2020-05-21 16:25:39 +0900

    時間軸が読み込めるように

--- lstm/1_Chickenpox/back2Future.m
+++ lstm/1_Chickenpox/back2Future.m
@@ -1,15 +1,16 @@
 %% 階段昇降動作の未来予測
 clear
-%% 系列データの読み込み
 
+jointindex = 25;
+load('timeindex.mat')
+%% 系列データの読み込み
+timeindex = timeindex';
 v3 = readmatrix("0520exData/training/0/pos.csv");
 data = lowpass(v3(:,2:97), 0.3);
 data = data';
-time = readmatrix("timeindex.csv");
 
 figure
-plot(data(1:4,:)');
-ylabel("Xvalue")
+plot(data(1:3,:)');
 title("Plevis")
 
 h = gca;
@@ -18,11 +19,12 @@ h.FontSize = 14;
 
 %% 学習データとテストデータの分割
 numTimeStepsTrain = floor(0.8 * size(data, 2));
+futurepoint = 30; %1秒後
 
 XTrain = data(:, 1:numTimeStepsTrain);
-YTrain = data(:, 10:numTimeStepsTrain+9);
-XTest = data(:, numTimeStepsTrain+1:end-9);
-YTest = data(:, numTimeStepsTrain+10:end);
+YTrain = data(:, 1+futurepoint:numTimeStepsTrain+futurepoint);
+XTest = data(:, numTimeStepsTrain+1:end-futurepoint);
+YTest = data(:, numTimeStepsTrain+1+futurepoint:end);
 
 numTimeStepsTest = size(XTest, 2);
 
@@ -43,7 +45,7 @@ XTest = (XTest - mu) ./ sig;
 
 inputSize = 96;
 numResponses = 96;
-numHiddenUnits = 200;
+numHiddenUnits = 300;
 
 layers = [ ...
     sequenceInputLayer(inputSize)
@@ -54,12 +56,12 @@ layers = [ ...
 %% 学習オプションの設定
 
 opts = trainingOptions('adam', ...
-    'MaxEpochs', 250, ...
+    'MaxEpochs', 300, ...
     'GradientThreshold', 1, ...
-    'InitialLearnRate', 0.005, ...
+    'InitialLearnRate', 0.01, ...
     'LearnRateSchedule', 'piecewise', ...
     'LearnRateDropPeriod', 125, ...
-    'LearnRateDropFactor', 0.2, ...
+    'LearnRateDropFactor', 0.3, ...
     'Verbose', 0, ...
     'Plots', 'training-progress');
 
@@ -91,29 +93,42 @@ YPred = sig .* YPred + mu;
 
 %% 予測誤差の算出
 
-rmse = sqrt(mean((YPred - YTest).^2));
-disp(rmse)
+rmse = sqrt(mean((YPred - YTest).^2, 2));
+% disp(rmse)
+
+%% 観測値・予測値の可視化
+
+% figure
+% 
+% plot(timeindex(idxTrain), data(jointindex, idxTrain))
+% hold on
+% 
+% plot(timeindex(idxTest), YPred(jointindex, :), '.-')
+% hold off
+% ylabel("value")
+% title("骨格座標の予測")
+% legend(["観測値" "予測値"])
 
 %% 観測値・予測値・予測誤差の可視化
 
 figure
 subplot(2, 1, 1)
-plot(time(idxTest), YTest(8,:))
+plot(timeindex(idxTest), YTest(jointindex,:))
 hold on
-plot(time(idxTest), YPred(8,:),'.-')
+plot(timeindex(idxTest), YPred(jointindex,:),'.-')
 hold off
 legend(["観測値" "予測値"])
 ylabel("value")
-title("骨格座標の予測値")
+title("一秒後の骨格座標の予測値")
 
 h = gca;
 h.FontName = 'Meiryo UI';
 h.FontSize = 14;
 
 subplot(2, 1, 2)
-stem(time(idxTest), YPred(8,:) - YTest(1,:))
+stem(timeindex(idxTest), YPred(jointindex,:) - YTest(jointindex,:))
 ylabel("予測誤差")
-title("RMSE = " + rmse)
+title("ave RMSE = " + mean(rmse))
 
 h = gca;
 h.FontName = 'Meiryo UI';
